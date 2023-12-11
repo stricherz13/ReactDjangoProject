0a1bdc80b305d9337fded082a3a42c32151dd95d
Brad Stricherz <Brad.Stricherz@gmail.com>
2023-12-11 16:46:30 -0600

    Second commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MySecondComponent from './Components/SecondComponent';
+
+function App() {
+    return (
+        <div>
+            <h1>John has {1 - 1} apples.</h1>
+        </div>
+    );
+}
+
+export default App;
