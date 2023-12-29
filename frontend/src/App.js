075373e59495402de9f2771cdb2a75698850da48
Brad Stricherz <Brad.Stricherz@gmail.com>
2023-12-29 16:05:07 -0600

    Updated components and started state management.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
-import MySecondComponent from './Components/SecondComponent';
+import AppleComponent from "./Components/AppleComponent";
+
+// const theFruits = [
+//     {id: 1, name: "Apple", color: "Red"},
+//     {id: 2, name: "Orange", color: "Orange"},
+//     {id: 3, name: "Banana", color: "Yellow"},
+//     {id: 4, name: "Kiwi", color: "Green"},
+//     {id: 5, name: "Mango", color: "Red"},
+//     {id: 6, name: "Avocado", color: "Green"},
+// ]
 
 function App() {
     return (
-        <div>
-            <h1>John has {1 - 1} apples.</h1>
-        </div>
+        <>
+           <AppleComponent/>
+        </>
     );
 }
 
---
+++ frontend/src/Components/Fruit.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Fruit(props) {
+  return <h1>This is my {props.color} {props.name} component</h1>;
+}
+
+export default Fruit;
