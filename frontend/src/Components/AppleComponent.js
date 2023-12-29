6ae5849ad29b7add77f1f06dd6c36b234b656dac
Brad Stricherz <Brad.Stricherz@gmail.com>
2023-12-29 16:25:49 -0600

    Update Apple component

--- frontend/src/Components/AppleComponent.js
+++ frontend/src/Components/AppleComponent.js
@@ -4,19 +4,31 @@ function AppleComponent() {
     const [numberOfApples, setNumberOfApples] = useState(0)
 
     function AppleDisplay(numberOfApples) {
-        if (numberOfApples === 0 || numberOfApples === 1) {
+        if (numberOfApples === 1) {
             return `John has ${numberOfApples} apple`
-        } else if (numberOfApples > 1) {
+        } else if (numberOfApples === 0 || numberOfApples > 1) {
             return `John has ${numberOfApples} apples`
         } else {
-            return `John owes is ${Math.abs(numberOfApples)} apples`
+            return `John owes us ${Math.abs(numberOfApples)} apples`
         }
     }
 
+    function IncreaseApple() {
+        setNumberOfApples((currentValue) => currentValue + 1)
+    }
+
+    function DecreaseApple() {
+        setNumberOfApples((currentValue) => currentValue - 1)
+    }
+
     return (
-        <div>
-            <h1>{AppleDisplay(numberOfApples)}</h1>
-        </div>
+        <>
+            <div>
+                <h1>{AppleDisplay(numberOfApples)}</h1>
+            </div>
+            <button onClick={IncreaseApple} className="add-btn">Increase</button>
+            <button onClick={DecreaseApple} className="decrease-btn">Decrease</button>
+        </>
     );
 }
 
