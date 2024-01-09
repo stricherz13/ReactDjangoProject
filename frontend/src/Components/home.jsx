42f17ba92c19b1781e91523ce062ef5271c4d909
Brad Stricherz <Brad.Stricherz@gmail.com>
2024-01-09 15:58:11 -0600

    Updated listings and login pages.

--- frontend/src/Components/home.jsx
+++ frontend/src/Components/home.jsx
@@ -1,10 +1,15 @@
 import React  from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div>
-      <h1>This is the home page</h1>
-    </div>
+      <div>
+          <h1>This is the home page</h1>
+          <br/>
+          <Link to='/login'>Login</Link>
+          <br/>
+          <Link to='listings'>Listings</Link>
+      </div>
   );
 }
 
