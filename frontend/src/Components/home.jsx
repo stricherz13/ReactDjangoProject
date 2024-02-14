388bbbedceff7e5d47c62b482308b4a217741e7d
Brad Stricherz <Brad.Stricherz@gmail.com>
2024-02-14 17:07:58 -0600

    Started to create the homepage

--- frontend/src/Components/home.jsx
+++ frontend/src/Components/home.jsx
@@ -1,16 +1,53 @@
-import React  from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import {Button, CssBaseline, Toolbar, AppBar, Typography} from "@mui/material";
+import {styled} from "@mui/material/styles"
+
+const useStyles = styled({
+    leftNav: {
+        marginRight: "auto"
+    },
+    rightNav: {
+        marginLeft: "auto",
+        marginRight: "10rem"
+    },
+    propertyBtn: {
+        backgroundColor: "green",
+        color: "white",
+        width: "15rem",
+        fontSize: "1.1rem",
+        marginRight: "1rem"
+    },
+    loginBtn: {
+        backgroundColor: "white",
+        color: "black",
+        width: "15rem",
+        fontSize: "1.1rem",
+        marginLeft: "1rem"
+    }
+});
 
 function Home() {
-  return (
-      <div>
-          <h1>This is the home page</h1>
-          <br/>
-          <Link to='/login'>Login</Link>
-          <br/>
-          <Link to='listings'>Listings</Link>
-      </div>
-  );
+    const classes = useStyles();
+    return (
+        <>
+            <CssBaseline/>
+            <AppBar position="static">
+                <Toolbar>
+                    <div className={classes.leftNav}>
+                        <Button color="inherit"><Typography variant="h4">LBREP</Typography></Button>
+                    </div>
+                    <div>
+                        <Button ><Typography variant="h6">Listings</Typography></Button>
+                        <Button ><Typography variant="h6">Agencies</Typography></Button>
+                    </div>
+                    <div className={classes.rightNav}>
+                        <Button className={classes.propertyBtn}>Add Property</Button>
+                        <Button className={classes.loginBtn}>Login</Button>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
 }
 
 export default Home;
